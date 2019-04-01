76b2e19cb695a3ff594b2e2e5bb1684a573f6e05
Andrey Murgin <murgin.andy@gmail.com>
2019-04-01 14:33:22 +0300

    Added couple of init tests

--- src/test/java/com/amur/fileMixer/cli/MixerCliTest.java
+++ src/test/java/com/amur/fileMixer/cli/MixerCliTest.java
@@ -1,6 +1,46 @@
 package com.amur.fileMixer.cli;
 
+import com.amur.fileMixer.api.Mixer;
+import com.amur.fileMixer.api.ParametersValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import picocli.CommandLine;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MixerCliTest {
 
-    //TODO Test Picocli correct MixerCli class initialization via CommandLine.populateCommand
+    private static ParametersValidator validator;
+    private static Mixer mixer;
+
+    @BeforeEach
+    void init() {
+        validator = Mockito.mock(ParametersValidator.class);
+        mixer = Mockito.mock(Mixer.class);
+    }
+
+    @Test
+    public void commandLineInitWithoutDirectoryTest() {
+        assertThrows(CommandLine.MissingParameterException.class,
+                () -> CommandLine.populateCommand(new MixerCli(mixer, validator)));
+    }
+
+    @Test
+    public void commandLineInitCorrectTest() {
+        MixerCli commandLine = CommandLine.populateCommand(new MixerCli(mixer, validator),
+                "./../resources/mixer-test");
+        assertNotNull(commandLine);
+        File directory = commandLine.getFilesDirectory();
+        assertNotNull(directory);
+        try {
+            System.out.println("Testing directory is " + directory.getCanonicalPath());
+        } catch (IOException e) {
+            fail("Unable to access test directory");
+        }
+        assertEquals(directory.getName(), "mixer-test");
+    }
 }
